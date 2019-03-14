0b4c53db0dd81a473bf97a5c545129fd2fa295e7
Sonahit <grandpajok@gmail.com>
2019-03-14 19:28:00 +0600

    UI complete

--- tic_tac_toe_UI/core/core.pas
+++ tic_tac_toe_UI/core/core.pas
@@ -10,13 +10,31 @@ uses
 type TCore = class
     private
       turn: integer;
-
+      field: array [0..2] of array [0..2] of integer;
     public
-    function whereInside(x,y: integer): string;
-
+      constructor Create;
+      function whereInside(x,y: integer): string;
+      function isEmpty(x,y: integer): boolean;
+      function isDraw: boolean;
+      function isWin: boolean;
+      procedure reset;
   end;
 
 implementation
+
+   constructor TCore.Create;
+   var
+      i,j: integer;
+   begin
+     for i:= 0 to length(field)-1  do
+        begin
+        for j:= 0 to length(field)-1 do
+           begin
+             field[i][j]:= 0;
+           end;
+        end;
+   end;
+
    function TCore.whereInside(x,y: integer): string;
    var
     s: string;
@@ -35,45 +53,290 @@ implementation
      str(turn, s);
      if TRectangle.create(TPoint.create(0, 8),TPoint.create(158,107)).isInside(x, y) then
        begin
-         Result:= '1' + s;
+         if isEmpty(x , y) then
+           begin
+             Result:= '1' + s;
+          end;
+         if (field[0 , 0] = 0) and (turn = 0) then
+           begin
+             field[0 , 0] := 1
+           end;
+         if (field[0 , 0] = 0) and (turn = 1) then
+           begin
+             field[0 , 0] := -1;
+           end;
        end;
-      if TRectangle.create(TPoint.create(158, 8),TPoint.create(336, 107)).isInside(x, y) then
+      if TRectangle.create(TPoint.create(158, 8),TPoint.create(336, 107)).isInside(x, y)  then
        begin
-         Result:= '2' +s;
+         if isEmpty(x , y) then
+           begin
+             Result:= '2' + s;
+           end;
+         if (field[0 , 1] = 0) and (turn = 0) then
+          begin
+           field[0 , 1] := 1
+          end;
+         if (field[0 , 1] = 0) and (turn = 1) then
+         begin
+           field[0 , 1] := -1;
+         end;
        end;
-      if TRectangle.create(TPoint.create(336, 8),TPoint.create(495, 107)).isInside(x, y) then
+      if TRectangle.create(TPoint.create(336, 8),TPoint.create(495, 107)).isInside(x, y)  then
        begin
-         Result:= '3' + s;
+         if isEmpty(x , y) then
+           begin
+             Result:= '3' + s;
+           end;
+         if (field[0 , 2] = 0) and (turn = 0) then
+          begin
+           field[0 , 2] := 1
+          end;
+         if (field[0 , 2] = 0) and (turn = 1) then
+         begin
+           field[0 , 2] := -1;
+         end;
        end;
-      if TRectangle.create(TPoint.create(0, 107),TPoint.create(158, 207)).isInside(x, y) then
+      if TRectangle.create(TPoint.create(0, 107),TPoint.create(158, 207)).isInside(x, y)  then
        begin
-         Result:= '4'+ s;
+         if isEmpty(x , y) then
+           begin
+             Result:= '4' + s;
+           end;
+         if (field[1 , 0] = 0) and (turn = 0) then
+          begin
+           field[1 , 0] := 1
+          end;
+         if (field[1 , 0] = 0) and (turn = 1) then
+         begin
+           field[1 , 0] := -1;
+         end;
        end;
-      if TRectangle.create(TPoint.create(158, 107),TPoint.create(336, 207)).isInside(x, y) then
+      if TRectangle.create(TPoint.create(158, 107),TPoint.create(336, 207)).isInside(x, y)  then
        begin
-         Result:= '5'+ s;
+         if isEmpty(x , y) then
+           begin
+             Result:= '5' + s;
+           end;
+         if (field[1 , 1] = 0) and (turn = 0) then
+          begin
+           field[1 , 1] := 1
+          end;
+         if (field[1 , 1] = 0) and (turn = 1) then
+          begin
+           field[1 , 1] := -1;
+          end;
        end;
       if TRectangle.create(TPoint.create(336, 107),TPoint.create(495, 207)).isInside(x, y) then
        begin
-         Result:= '6'+ s;
+        if isEmpty(x , y) then
+           begin
+             Result:= '6' + s;
+           end;
+         if (field[1 , 2] = 0) and (turn = 0) then
+          begin
+           field[1 , 2] := 1
+          end;
+         if (field[1 , 2] = 0) and (turn = 1) then
+         begin
+           field[1 , 2] := -1;
+         end;
+       end;
+      if TRectangle.create(TPoint.create(0, 207),TPoint.create(158, 310)).isInside(x, y)  then
+       begin
+         if isEmpty(x , y) then
+           begin
+             Result:= '7' + s;
+           end;
+         if (field[2 , 0] = 0) and (turn = 0) then
+          begin
+           field[2 , 0] := 1
+          end;
+         if (field[2 , 0] = 0) and (turn = 1) then
+         begin
+           field[2 , 0] := -1;
+         end;
+       end;
+      if TRectangle.create(TPoint.create(158, 207),TPoint.create(336, 310)).isInside(x, y)  then
+       begin
+         if isEmpty(x , y) then
+           begin
+             Result:= '8' + s;
+           end;
+         if (field[2 , 1] = 0) and (turn = 0) then
+          begin
+           field[2 , 1] := 1
+          end;
+         if (field[2 , 1] = 0) and (turn = 1) then
+         begin
+           field[2, 1] := -1;
+         end;
+       end;
+      if TRectangle.create(TPoint.create(336, 207),TPoint.create(495, 310)).isInside(x, y)  then
+       begin
+          if isEmpty(x , y) then
+           begin
+             Result:= '9' + s;
+           end;
+         if (field[2 , 2] = 0) and (turn = 0)  then
+          begin
+           field[2 , 2] := 1
+          end;
+         if (field[2 , 2] = 0) and (turn = 1) then
+         begin
+           field[2 , 2] := -1;
+         end;
+       end;
+      if (turn = 0) and (Result <> '') then
+        turn := 1
+      else
+        begin
+          if (turn = 1) and (Result <> '') then
+             turn:= 0;
+        end;
+      if Result = '' then
+         Result:= '00';
+   end;
+
+   function TCore.isEmpty(x,y: integer): boolean;
+   begin
+     Result:= false;
+      if TRectangle.create(TPoint.create(0, 8),TPoint.create(158,107)).isInside(x, y) then
+       begin
+          if field[0][0] = 0 then
+            begin
+              Result:= true;
+            end;
+       end;
+      if TRectangle.create(TPoint.create(158, 8),TPoint.create(336, 107)).isInside(x, y) then
+       begin
+         if field[0][1] = 0 then
+            begin
+              Result:= true;
+            end;
+       end;
+      if TRectangle.create(TPoint.create(336, 8),TPoint.create(495, 107)).isInside(x, y) then
+       begin
+         if field[0][2] = 0 then
+            begin
+              Result:= true;
+            end;
+       end;
+      if TRectangle.create(TPoint.create(0, 107),TPoint.create(158, 207)).isInside(x, y) then
+       begin
+         if field[1][0] = 0 then
+            begin
+              Result:= true;
+            end;
+       end;
+      if TRectangle.create(TPoint.create(158, 107),TPoint.create(336, 207)).isInside(x, y) then
+       begin
+         if self.field[1][1] = 0 then
+            begin
+              Result:= true;
+            end;
+       end;
+      if TRectangle.create(TPoint.create(336, 107),TPoint.create(495, 207)).isInside(x, y) then
+       begin
+        if field[1][2] = 0 then
+            begin
+              Result:= true;
+            end;
        end;
       if TRectangle.create(TPoint.create(0, 207),TPoint.create(158, 310)).isInside(x, y) then
        begin
-         Result:= '7'+s;
+         if field[2][0] = 0 then
+            begin
+              Result:= true;
+            end;
        end;
       if TRectangle.create(TPoint.create(158, 207),TPoint.create(336, 310)).isInside(x, y) then
        begin
-         Result:= '8'+ s;
+         if field[2][1] = 0 then
+            begin
+              Result:= true;
+            end;
        end;
       if TRectangle.create(TPoint.create(336, 207),TPoint.create(495, 310)).isInside(x, y) then
        begin
-         Result:= '9'+ s;
+         if field[2][2] = 0 then
+            begin
+              Result:= true;
+            end;
        end;
-      if turn = 0 then
-        turn := 1
-      else
-        turn:= 0;
    end;
 
+   function TCore.isDraw: boolean;
+   var
+      i,j: integer;
+   begin
+     Result:= true;
+     for i:= 0 to 2 do
+        for j:= 0 to 2 do
+           begin
+             if (field[i][j] = 0) then
+                begin
+                  Result:= false;
+                  break;
+                end;
+           end;
+   end;
+
+   function TCore.isWin: boolean;
+   var i,j: integer;
+   begin
+     i:= 0;
+     Result:= false;
+        for j:= 0 to 2 do
+           begin
+             if ((field[i][j] + field[i+1][j] + field[i+2][j]) = 3) or
+                ((field[i][j] + field[i+1][j] + field[i+2][j]) = -3)
+             then
+                begin
+                  Result:= true;
+                  break;
+                end;
+           end;
+        j:= 0;
+        for i:= 0 to 2 do
+           begin
+             if ((field[i][j] + field[i][j+1] + field[i][j+2]) = 3) or
+                ((field[i][j] + field[i][j+1] + field[i][j+2]) = -3)
+                then
+                begin
+                  Result:= true;
+                  break;
+                end;
+           end;
+        i:= 0;
+        j:= 0;
+        if ((field[i][j] + field[i+1][j+1] + field[i+2][j+2]) = 3) or
+           ((field[i][j+2] + field[i+1][j+1] + field[i+2][j]) = 3)
+           then
+                begin
+                  Result:= true;
+                end;
+        if ((field[i][j] + field[i+1][j+1] + field[i+2][j+2]) = -3) or
+           ((field[i][j+2] + field[i+1][j+1] + field[i+2][j]) = -3)
+           then
+                begin
+                  Result:= true;
+                end;
+   end;
+
+   procedure TCore.reset;
+   var
+      i,j: integer;
+   begin
+     for i:= 0 to length(field)-1  do
+        begin
+        for j:= 0 to length(field)-1 do
+           begin
+             field[i][j]:= 0;
+           end;
+        end;
+     turn:= 0;
+   end;
 end.
+
+
 
--- tic_tac_toe_UI/unit1.pas
+++ tic_tac_toe_UI/unit1.pas
@@ -14,6 +14,7 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     Edit1: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
@@ -21,14 +22,14 @@ type
     Label2: TLabel;
     PaintBox1: TPaintBox;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Label2Click(Sender: TObject);
     procedure PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
   private
 
     procedure makeField;
-    procedure drawTurn(s: string; x, y: integer);
+    procedure drawTurn(s: string);
   public
 
   end;
@@ -36,6 +37,7 @@ type
 var
   Form1: TForm1;
   coreImpl: TCore;
+  locked: boolean;
 implementation
 
 {$R *.lfm}
@@ -45,6 +47,8 @@ implementation
  procedure TForm1.FormCreate(Sender: TObject);
 begin
   Button1.Caption:= 'Start';
+  Button2.Caption:= 'Restart';
+  Button2.Visible:= false;
   Edit1.text:= 'Enter your name';
   Edit2.visible:= false;
   label1.visible:= false;
@@ -54,11 +58,7 @@ begin
   Constraints.MaxWidth:= Width;
   Constraints.MinWidth:= Width;
   Constraints.MinHeight:= Height;
-end;
-
-procedure TForm1.Label2Click(Sender: TObject);
-begin
-
+  locked:= true;
 end;
 
 
@@ -68,10 +68,32 @@ var
   s: string;
   pCore: ^TCore;
 begin
+  if locked = true then
+   begin
    pCore:= @coreImpl;
    s:= pCore^.whereInside(x,y);
-   drawTurn(s, x ,y);
-
+   if s[1] <> '0' then
+     begin
+     drawTurn(s);
+     if pCore^.isWin then
+        begin
+         Label2.Visible:= true;
+         if s[2] = '0' then
+           Label2.Caption:= 'Player ' + Edit2.Text + ' Won!'
+         else
+           Label2.Caption:= 'Player ' + Edit3.Text + ' Won!';
+         Label2.Color:= clYellow;
+         locked:= false;
+       end;
+     if pCore^.isDraw then
+       begin
+         Label2.Visible:= true;
+         Label2.Caption:= 'Draw!';
+         Label2.Color:= clRed;
+         locked:= false;
+       end;
+     end;
+  end;
 end;
  {
     Rectangle 1 = (0, 8 ; 158, 107)
@@ -80,11 +102,11 @@ end;
     Rectangle 4 = (0, 107+34; 158, 207+34)
     Rectangle 5 = (158, 107+34; 336, 207+34)
     Rectangle 6 = (336, 107+34; 495, 207+34)
-    Rectangle 7 = (0, 207+34; 158, 310+34)
-    Rectangle 8 = (158, 207+34; 336, 310+34)
-    Rectangle 9 = (336, 207+34; 495, 310+34)
+    Rectangle 7 = (0, 207+34, 158, 310+34)
+    Rectangle 8 = (158, 207+34, 336, 310+34)
+    Rectangle 9 = (336, 207+34, 495, 310+34)
    }
-procedure TForm1.drawTurn(s: string; x, y: integer);
+procedure TForm1.drawTurn(s: string);
 begin
   case s[1] of
   '1': begin
@@ -106,84 +128,84 @@ begin
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(336+56, 40, 158+56, 141);
         end;
     end;
     '3': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(336+56, 40, 495+56, 141);
+          Canvas.Line(495+56, 40 , 336+56, 141);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(336+56, 40, 495+56, 141);
         end;
     end;
   '4': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(0+56, 107+34, 158+56, 207+34);
+          Canvas.Line(158+56, 107+34, 0+56, 207+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(0+56, 107+34, 158+56, 207+34);
         end;
     end;
     '5': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(158+56, 107+34, 336+56, 207+34);
+          Canvas.Line(336+56, 107+34, 158+56, 207+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(158+56, 107+34, 336+56, 207+34);
         end;
     end;
   '6': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(336+56, 107+34, 495+56, 207+34);
+          Canvas.Line(495+56, 107+34, 336+56, 207+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(336+56, 107+34, 495+56, 207+34);
         end;
     end;
     '7': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(0+56, 207+34, 158+56, 310+34);
+          Canvas.Line(158+56, 207+34, 0+56, 310+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(0+56, 207+34, 158+56, 310+34);
         end;
     end;
   '8': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(158+56, 207+34, 336+56, 310+34);
+          Canvas.Line(336+56, 207+34, 158+56, 310+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(158+56, 207+34, 336+56, 310+34);
         end;
     end;
     '9': begin
       if s[2] = '0' then
         begin
-          Canvas.Line(56,40, 158+56, 141);
-          Canvas.Line(158+56, 40 , 0+56, 141);
+          Canvas.Line(336+56, 207+34, 495+56, 310+34);
+          Canvas.Line(495+56, 207+34, 336+56, 310+34);
         end
       else
         begin
-        canvas.Ellipse(56,40, 158+56, 141);
+        canvas.Ellipse(336+56, 207+34, 495+56, 310+34);
         end;
     end;
 
@@ -202,6 +224,16 @@ begin
   edit3.visible:= true;
   makeField;
   coreImpl:= TCore.Create;
+  Button2.Visible:= true;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Label2.Visible:= false;
+  makeField;
+  coreImpl.reset;
+  locked:= true;
+
 end;
 
 procedure TForm1.makeField;
